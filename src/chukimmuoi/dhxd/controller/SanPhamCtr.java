018b28c66ab24bbead8be74725cae4a47ad1c611
chukimmuoi <chukimmuoi@gmail.com>
2015-08-20 17:35:59 +0700

    Version 3

--- src/chukimmuoi/dhxd/controller/SanPhamCtr.java
+++ src/chukimmuoi/dhxd/controller/SanPhamCtr.java
@@ -34,6 +34,25 @@ public class SanPhamCtr implements ISanPham{
 	@Override
 	public void insertSanPham(SanPham insert) throws SQLException {
 		this.sanphamDao.create(insert);
+		
+//		TransactionManager.callInTransaction(sanphamDao.getConnectionSource(), new java.util.concurrent.Callable<SanPhamDao>() {
+//			@Override
+//			public SanPhamDao call() throws Exception {
+//				SanPham obj = new SanPham();
+//				obj.setGia(60000);
+//				obj.setMoTa("Tiền");
+//				obj.setSoLuong(1);
+//				long start = System.currentTimeMillis();
+//				for (int i = 0; i < 1000; i++) {
+//					obj.setGia(i);
+//					sanphamDao.create(obj);
+//				}
+//				long end = System.currentTimeMillis();
+//				System.out.println("MMMMMMMMMMMMMMMMMMMM: " + (end - start));
+//				return null;
+//			}
+//		});
+//		this.sanphamDao.create(insert);
 	}
 
 	@Override
